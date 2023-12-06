457921291b6404470056ade19941772370d5f597
Wesley Lima <weeesleylima@hotmail.com>
2023-12-05 21:41:28 -0300

    feat: Listando cursos

---
+++ crud-spring/src/main/java/com/wesley/controller/CourseController.java
@@ -0,0 +1,32 @@
+package com.wesley.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wesley.model.Course;
+import com.wesley.repository.CourseRepository;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    
+    
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+}
