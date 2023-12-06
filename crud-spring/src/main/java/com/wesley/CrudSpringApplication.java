12a5604d746318bb1a0d7258e65d241baad94c1d
Wesley Lima <weesleylima@hotmail.com>
2023-12-05 21:43:34 -0300

    .

--- crud-spring/src/main/java/com/wesley/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/wesley/CrudSpringApplication.java
@@ -22,7 +22,7 @@ public class CrudSpringApplication {
 			
 			Course c = new Course();
 			c.setName("Angular com Spring");
-			c.setCategory("Front-End");
+			c.setCategory("front-end");
 			
 			courseRepository.save(c);
 		};
